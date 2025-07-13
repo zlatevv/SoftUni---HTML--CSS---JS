cd9b8356f5d8d91ff4be6959a12c18b8c53845f5
ExamPrep <cvetanzlatev30@gmail.com>
2025-07-13 22:07:03 +0300

    DOM Introduction exercise

--- part-2/DOM-Introduction/exercise/02. Pascal or Camel Case/pascal-or-camel-case.js
+++ part-2/DOM-Introduction/exercise/02. Pascal or Camel Case/pascal-or-camel-case.js
@@ -1,21 +1,47 @@
 function solve() {
     const text = document.getElementById("text").value;
-    const typeCase = document.getElementById("naming-convention").value
-    const result = document.getElementById("result")
+    const typeCase = document.getElementById("naming-convention").value;
+    const result = document.getElementById("result");
 
-    switch (typeCase){
-      case "Camel Case":
-        let words = text.split(" ")
-        
+    result.textContent = "";
 
-        break;
-      case "Pascal Case":
+    switch (typeCase) {
+        case "Camel Case":
+            {
+                const words = text.split(" ");
+                let tempResult = "";
 
+                for (let i = 0; i < words.length; i++) {
+                    let word = words[i].toLowerCase();
+                    if (word === "") continue;
 
+                    if (i !== 0) {
+                        word = word[0].toUpperCase() + word.slice(1);
+                    }
+                    tempResult += word;
+                }
 
-        break;
-      default:
-        result.textContent += "Error!"
+                result.textContent = tempResult;
+            }
+            break;
+
+        case "Pascal Case":
+            {
+                const words = text.split(" ");
+                let tempResult = "";
+
+                for (let word of words) {
+                    word = word.toLowerCase();
+                    if (word === "") continue;
+
+                    tempResult += word[0].toUpperCase() + word.slice(1);
+                }
+
+                result.textContent = tempResult;
+            }
+            break;
+
+        default:
+            result.textContent = "Error!";
     }
-    
 }
