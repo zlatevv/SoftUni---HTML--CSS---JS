8f1be690d98fe83aa6305f7051567684a6e6a496
ExamPrep <cvetanzlatev30@gmail.com>
2025-07-14 14:34:49 +0300

    DOM Introduction exercise

--- part-2/DOM-Introduction/exercise/03. Accordion/accordion.js
+++ part-2/DOM-Introduction/exercise/03. Accordion/accordion.js
@@ -1,3 +1,16 @@
 function toggle() {
-    //TODO
+    const button = document.getElementsByClassName("button")[0]
+    const content = document.getElementById("extra")
+    let text = button.textContent;
+
+    switch (text){
+        case "More":
+            button.textContent = "Less"
+            content.style.display = "block"
+            break;
+        case "Less":
+            button.textContent = "More"
+            content.style.display = "none"
+            break;
+    }
 }
--- part-2/DOM-Introduction/exercise/04. Search in List/search-in-list.js
+++ part-2/DOM-Introduction/exercise/04. Search in List/search-in-list.js
@@ -1,3 +1,15 @@
 function solve() {
-   // TODO
+   const towns = document.querySelectorAll("#towns li")
+   const input = document.getElementById("searchText").value
+   const result = document.getElementById("result")
+
+   let matches = 0
+   for (const town of towns) {
+      if (town.textContent.includes(input)){
+         town.style.fontWeight = "bold" 
+         town.style.textDecoration = "underline"
+         matches++
+      }
+   }
+   result.textContent = `${matches} matches found`
 }
