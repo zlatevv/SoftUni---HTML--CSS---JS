a151dd1193abc86d0c8062c7736b55f24022b01f
ExamPrep <cvetanzlatev30@gmail.com>
2025-07-16 13:08:43 +0300

    DOM Introduction exercise

--- part-2/DOM-Introduction/exercise/08. Generate Report/generate-report.js
+++ part-2/DOM-Introduction/exercise/08. Generate Report/generate-report.js
@@ -1,3 +1,24 @@
 function solve() {
-    //TODO
+    let input = document.querySelectorAll('input');
+
+    let tableData = document.querySelectorAll('tbody > tr');
+    
+    let result = [];
+    for (let row of tableData) {
+        object = {};
+        let cells = row.querySelectorAll('td');
+
+        for (let i = 0; i < cells.length; i++) {
+            if (input[i].checked) {
+                let columnName = input[i].parentElement.textContent.toLowerCase().trim();
+                object[columnName] = cells[i].textContent.trim();
+            }
+        }
+
+        result.push(object);
+    }
+
+    let output = document.getElementById('output');
+
+    output.value = JSON.stringify(result);
 }
--- part-2/DOM-Introduction/exercise/09. Number Convertor/number-convertor.js
+++ part-2/DOM-Introduction/exercise/09. Number Convertor/number-convertor.js
@@ -1,3 +1,17 @@
 function solve() {
-    // TODO
+    const input = document.getElementById("input").value
+    const toConvertEl = document.getElementById("selectMenuTo").value
+    const result = document.getElementById("result")
+
+    let number = Number(input)
+    if (!input){
+        return
+    }
+    switch (toConvertEl){
+        case "binary":
+            result.value = number.toString(2)
+            break;
+        case "hexadecimal":
+            result.value = number.toString(16).toUpperCase()
+    }
 }
