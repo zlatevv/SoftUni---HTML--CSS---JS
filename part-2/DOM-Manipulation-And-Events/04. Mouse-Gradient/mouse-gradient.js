e2cb5be1dd458cc454b41878a75fc2b4c637be65
zlatevv <cvetanzlatev30@gmail.com>
2025-07-18 14:14:59 +0300

    DOM Manipulation And Events lab

--- part-2/DOM-Manipulation-And-Events/04. Mouse-Gradient/mouse-gradient.js
+++ part-2/DOM-Manipulation-And-Events/04. Mouse-Gradient/mouse-gradient.js
@@ -1,3 +1,15 @@
 function attachGradientEvents() {
-    //TODO
+    const gradientEl = document.getElementById("gradient")
+    const resultEl = document.getElementById("result")
+
+    gradientEl.addEventListener("mousemove", handleMovement)
+
+
+    function handleMovement(e){
+        let percentages = e.offsetX / e.target.clientWidth * 100
+
+        resultEl.textContent = Math.floor(percentages) + "%"
+        
+        
+    }
 }
