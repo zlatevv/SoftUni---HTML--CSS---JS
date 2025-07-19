eb2af41cb7e34e569af953b21f8ecc3cab39ddc5
zlatevv <cvetanzlatev30@gmail.com>
2025-07-19 10:15:03 +0300

    DOM Manipulation And Events lab

--- part-2/DOM-Manipulation-And-Events/06. Dynamic-Validation/dynamic-validation.js
+++ part-2/DOM-Manipulation-And-Events/06. Dynamic-Validation/dynamic-validation.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    let pattern = /^[a-z]+@[a-z]+\.[a-z]+$/
+    const input = document.getElementById("email")
+    
+    input.addEventListener("change", handleInput)
+    
+
+    function handleInput(){
+        if (pattern.test(input.value)){
+            input.classList.remove("error")
+        }else {
+            input.classList.add("error")
+        }
+    }
 }
