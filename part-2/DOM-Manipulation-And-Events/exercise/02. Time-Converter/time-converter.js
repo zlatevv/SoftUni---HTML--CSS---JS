7064dc29a5a52335e80b2732d0582838673cc929
zlatevv <cvetanzlatev30@gmail.com>
2025-07-22 13:07:07 +0300

    DOM Manipulation And Events exercise

--- part-2/DOM-Manipulation-And-Events/exercise/02. Time-Converter/time-converter.js
+++ part-2/DOM-Manipulation-And-Events/exercise/02. Time-Converter/time-converter.js
@@ -1,5 +1,42 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const forms = document.querySelectorAll("form")
+    const days = document.getElementById("days-input")
+    const hours = document.getElementById("hours-input")
+    const minutes = document.getElementById("minutes-input")
+    const seconds = document.getElementById("seconds-input")
+    
+    forms.forEach(form => form.addEventListener("submit", handleSubmition))
+
+    function handleSubmition(e){
+        e.preventDefault()
+        const clickedTarget = e.target
+        
+        switch (clickedTarget.id){
+            case "days":
+                const daysValue = days.value 
+                hours.value = (daysValue * 24).toFixed(2)
+                minutes.value = (daysValue * 1440).toFixed(2)
+                seconds.value = (daysValue * 86400).toFixed(2)
+                break;
+            case "hours":
+                const hoursValue = hours.value
+                days.value = (hoursValue / 24).toFixed(2)
+                minutes.value = (hoursValue * 60).toFixed(2)
+                seconds.value = (hoursValue * 3600).toFixed(2)
+                break;
+            case "minutes":
+                const minutesValue = minutes.value
+                days.value = (minutesValue / 1440).toFixed(2)
+                hours.value = (minutesValue / 60).toFixed(2)
+                seconds.value = (minutesValue * 60).toFixed(2)
+                break
+            case "seconds":
+                const secondsValue = seconds.value
+                days.value = (secondsValue / 86400).toFixed(2)
+                hours.value = (secondsValue / 3600).toFixed(2)
+                minutes.value = (secondsValue / 60).toFixed(2)
+        }
+    }
 }
--- part-2/DOM-Manipulation-And-Events/exercise/03. Locked-Profile/locked-profile.js
+++ part-2/DOM-Manipulation-And-Events/exercise/03. Locked-Profile/locked-profile.js
@@ -1,5 +1,26 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const buttons = document.querySelectorAll("button")
+
+    buttons.forEach(button => button.addEventListener("click", handleLock))
+
+    function handleLock(e){
+        const fullElement = e.target.parentElement;
+        const radioEl = fullElement.querySelector("input[type=radio]")
+
+        if (radioEl.checked){
+            return  
+        }
+
+        const hiddenFields = fullElement.querySelector(".hidden-fields")
+
+        if (e.target.textContent === "Show more"){
+            hiddenFields.style.display = 'block'
+            e.target.textContent = "Show less"
+        }else if (e.target.textContent === "Show less"){
+            hiddenFields.style.display = 'none'
+            e.target.textContent = "Show more"
+        }
+    }
 }
