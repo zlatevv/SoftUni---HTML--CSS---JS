73952e53f789809ff990c9a133f9b34c393308ab
ExamPrep <cvetanzlatev30@gmail.com>
2025-07-30 15:48:55 +0300

    moved folders

--- part-2/HTTP-REST-And-AJAX/lab/02. GitHub-Repos/scripts/app.js
+++ part-2/HTTP-REST-And-AJAX/lab/02. GitHub-Repos/scripts/app.js
@@ -1,3 +1,10 @@
 function loadRepos() {
-   console.log("TODO...");
+   const resultEl = document.getElementById("res")
+
+   fetch("https://api.github.com/users/testnakov/repos")
+         .then(response => response.text())
+         .then(data => {
+            resultEl.textContent = data
+         })
+
 }
