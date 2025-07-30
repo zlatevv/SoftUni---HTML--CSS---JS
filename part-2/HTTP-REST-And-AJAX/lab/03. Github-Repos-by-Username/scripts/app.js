0cdfea8716886b2cab6aa1c917d80937727536d2
ExamPrep <cvetanzlatev30@gmail.com>
2025-07-30 15:58:16 +0300

    feat. github repos by username load onclick

--- part-2/HTTP-REST-And-AJAX/lab/03. Github-Repos-by-Username/scripts/app.js
+++ part-2/HTTP-REST-And-AJAX/lab/03. Github-Repos-by-Username/scripts/app.js
@@ -1,3 +1,29 @@
 function loadRepos() {
-	console.log("TODO...");
+	const username = document.getElementById('username').value;
+	const repos = document.getElementById('repos');
+	repos.innerHTML = '';
+
+		try{
+			fetch(`https://api.github.com/users/${username}/repos`)
+				.then(response => {
+					if (!response.ok) {
+						throw new Error('Network response was not ok');
+					}
+					return response.json();
+				})
+				.then(allRepos => {
+					allRepos.forEach(repo => {
+						const liEl = document.createElement('li');
+						const aEl = document.createElement('a');
+						aEl.href = repo.html_url;
+						aEl.textContent = repo.full_name;
+
+						liEl.appendChild(aEl);
+						repos.appendChild(liEl);
+					});
+				})
+		}
+		catch (error){
+			console.log(`Error: ${error.status} (Not Found)`);
+		}
 }
