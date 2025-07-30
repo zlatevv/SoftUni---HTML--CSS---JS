f6436a21271cd597a6771847f2bbc585656a8c15
ExamPrep <cvetanzlatev30@gmail.com>
2025-07-30 16:38:23 +0300

    feat. loading github commits on click

--- part-2/HTTP-REST-And-AJAX/lab/04. Github-Commits/app.js
+++ part-2/HTTP-REST-And-AJAX/lab/04. Github-Commits/app.js
@@ -1,4 +1,24 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    const username = document.getElementById('username').value;
+    const repo = document.getElementById('repo').value;
+    const commits = document.getElementById('commits');
+    commits.innerHTML = '';
+
+    fetch(`https://api.github.com/repos/${username}/${repo}/commits`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status} (Not Found)`);
+            }
+            return response.json();
+        })
+        .then(commits => {
+            commits.forEach(commit => {
+                const liEl = document.createElement('li');
+                liEl.textContent = `${commit.commit.author.name}: ${commit.commit.message}`;
+                document.getElementById('commits').appendChild(liEl);
+            });
+        })
+        .catch(error => {
+            commits.innerHTML = error.message
+    });
 }
