df16cc57d6d23c5862ef51d3a62c491e5b8c9db5
ExamPrep <cvetanzlatev30@gmail.com>
2025-07-31 16:22:41 +0300

    feat. bus stop name and buses loading onclick with correct id

--- part-2/HTTP-REST-And-AJAX/lab/AJAX/01.Bus-Stop/app.js
+++ part-2/HTTP-REST-And-AJAX/lab/AJAX/01.Bus-Stop/app.js
@@ -1,3 +1,26 @@
 function getInfo() {
-    console.log("TODO...");
+    const stopId = document.getElementById('stopId').value;
+    const stopName = document.getElementById('stopName');
+    const busesList = document.getElementById('buses');
+
+    fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopId}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error fetching bus information');
+            }
+            return response.json();
+        })
+        .then(busStopInformation => {
+            const buses = busStopInformation.buses;
+            stopName.textContent = busStopInformation.name;
+            busesList.innerHTML = '';
+            for (const [busID, time] of Object.entries(buses)) {
+                const liItem = document.createElement('li');
+                liItem.textContent = `Bus ${busID} arrives in ${time} minutes`;
+                busesList.appendChild(liItem);
+            }
+        })
+        .catch(error => {
+            stopName.textContent = 'Error';
+        })
 }
