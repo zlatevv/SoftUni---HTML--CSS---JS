e292b98921149cbdd36c2f4a237dfbb078d45489
ExamPrep <cvetanzlatev30@gmail.com>
2025-07-31 17:06:00 +0300

    feat. arrive and depart

--- part-2/HTTP-REST-And-AJAX/lab/AJAX/02.Bus-Schedule/app.js
+++ part-2/HTTP-REST-And-AJAX/lab/AJAX/02.Bus-Schedule/app.js
@@ -1,11 +1,34 @@
 function solve() {
-
+    let id = "depot"
+    const infoElement = document.querySelector('.info');   
+    const departButton = document.querySelector('#depart');
+    const arriveButton = document.querySelector('#arrive');
+    
     function depart() {
-        console.log('Depart TODO...');
+
+        fetch(`http://localhost:3030/jsonstore/bus/schedule/${id}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                infoElement.textContent = `Next stop ${data.name}`;
+                departButton.disabled = true;
+                arriveButton.disabled = false;
+                id = data.next;
+            })
+            .catch(error => {
+                console.error('Error fetching bus schedule:', error);
+            });
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        arriveButton.disabled = true;
+        departButton.disabled = false;
+        infoElement.textContent = `Arriving at ${id}`;
+        id = "depot";
     }
 
     return {
