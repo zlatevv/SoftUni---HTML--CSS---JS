03e9e4cce778649a1d272542af5df7b4691078de
ExamPrep <cvetanzlatev30@gmail.com>
2025-08-02 15:41:16 +0300

    feat. requests sent and used successfully

--- part-2/HTTP-REST-And-AJAX/exercise/02.Messenger/app.js
+++ part-2/HTTP-REST-And-AJAX/exercise/02.Messenger/app.js
@@ -1,5 +1,35 @@
-function attachEvents() {
-    console.log('TODO...');
-}
+const messagesTextArea = document.getElementById("messages")
+const authorEl = document.querySelector("input[name='author']")
+const messageEl = document.querySelector("input[name='content']")
+const submitBtn = document.getElementById("submit")
+const refreshBtn = document.getElementById("refresh")
 
+function attachEvents() {
+    submitBtn.addEventListener("click", handleSubmition)
+    refreshBtn.addEventListener("click", handleRefreshing)
+}
+async function handleSubmition() {
+    const author = authorEl.value.trim()
+    const content = messageEl.value.trim()
+
+    const message = { author, content }
+
+    const result = await fetch(`http://localhost:3030/jsonstore/messenger`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(message)
+    })
+}
+async function handleRefreshing() {
+    const result = await fetch(`http://localhost:3030/jsonstore/messenger`)
+    const data = await result.json()
+
+    let messages = []
+    for (const message of Object.values(data)) {
+        messages.push(`${message.author}: ${message.content}`)
+    }
+    messagesTextArea.innerHTML = messages.join("\n")
+}
 attachEvents();
