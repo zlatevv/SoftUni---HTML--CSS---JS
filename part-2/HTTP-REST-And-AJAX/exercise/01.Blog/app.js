d8f0788e98b6d5b1e049b659a44ecd221db4189d
ExamPrep <cvetanzlatev30@gmail.com>
2025-08-02 14:59:32 +0300

    feat. post and comments loading, 1 test does not pass

--- part-2/HTTP-REST-And-AJAX/exercise/01.Blog/app.js
+++ part-2/HTTP-REST-And-AJAX/exercise/01.Blog/app.js
@@ -1,5 +1,48 @@
+const btnLoadPosts = document.getElementById("btnLoadPosts")
+const selectPostsEl = document.getElementById("posts")
+const btnViewPosts = document.getElementById("btnViewPost")
+const postBody = document.getElementById("post-body")
+const postComments = document.getElementById("post-comments")
+
 function attachEvents() {
-    console.log('TODO...');
+    btnLoadPosts.addEventListener("click", handleLoadingPosts)
+    btnViewPosts.addEventListener("click", handleViewingPosts)
 }
 
+async function handleLoadingPosts(){
+    const result = await fetch(`http://localhost:3030/jsonstore/blog/posts`)
+    const data = await result.json()  
+    for (const element of Object.values(data)) {
+        const optionEl = document.createElement("option")
+        optionEl.value = element.id
+        optionEl.textContent = element.title
+        selectPostsEl.appendChild(optionEl)
+    }
+}
+async function handleViewingPosts(){
+    const result = await fetch(`http://localhost:3030/jsonstore/blog/posts`)
+    const data = await result.json()  
+    const option = selectPostsEl.value 
+
+    for (const [id, values] of Object.entries(data)) {
+        if (id == option){
+            postBody.textContent = values.body
+            handleCommentsShowcasing(id)
+        }
+    }
+}
+async function handleCommentsShowcasing(id){
+    const result = await fetch(`http://localhost:3030/jsonstore/blog/comments`)
+    const data = await result.json() 
+
+    const texts = Object.values(data)
+        .filter(object => object.postId == id)
+        .forEach(object => {
+            const liEl = document.createElement("li")
+            liEl.textContent = object.text
+            liEl.id = object.id
+            postComments.appendChild(liEl)
+        })
+
+}
 attachEvents();
